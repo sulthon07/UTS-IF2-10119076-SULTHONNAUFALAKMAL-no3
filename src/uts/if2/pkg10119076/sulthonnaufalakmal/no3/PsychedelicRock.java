9c9ed34987f6a25ca52431b656cb412608b35765
ACER <ACER@DESKTOP-JMQ2UKH>
2020-12-04 21:33:21 +0700

    UTS-IF2-10119076-SULTHONNAUFALAKMAL-no3

---
+++ src/uts/if2/pkg10119076/sulthonnaufalakmal/no3/PsychedelicRock.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119076.sulthonnaufalakmal.no3;
+
+/**
+ *
+ * @author ACER
+ */
+public interface PsychedelicRock {
+    
+}
---
+++ src/uts/if2/pkg10119076/sulthonnaufalakmal/no3/UTSIF210119076SULTHONNAUFALAKMALNo3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if2.pkg10119076.sulthonnaufalakmal.no3;
+
+/**
+ *
+ * @author ACER
+ */
+public class UTSIF210119076SULTHONNAUFALAKMALNo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
